5141a019e00d3464b54b764f226d1ac0e5544d83
Neethu Joseph <neethujo@gmail.com>
2017-01-22 16:09:58 -0600

    implemented fortune cookie with fortune and lucky number

--- main.py
+++ main.py
@@ -15,10 +15,36 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    #list of fortunes
+    fortunes = [
+    "All things are difficult before they are easy",
+    "The fortune you seek ,is in another cookie",
+    "I cannot help you much ,I'm just a cookie",
+    "The early bird get the worm, but second mouse gets the cheese",
+    "Meh 404 Fortune not found ,try another"
+    ]
+    #randomly select one fortune to display
+    index = random.randint(0,4)
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+        fortune = "<strong>" +getRandomFortune()+ "</strong>"
+        fortune_sentence= "Your Fortune: "+ fortune
+        fortune_paragraph = "<p>" +fortune_sentence+ "</p>"
+
+        refresh_button = "<a href ='.'><button> Another Cookie Please !! </button></a>"
+
+        lucky_number = "<strong>" + str(random.randint(1,100))+ "</strong>"
+        num_sentence = "Your Lucky number is: "+str(lucky_number)
+        num_paragraph = "<p>" +num_sentence+ "</p>"
+        content = header +num_paragraph +fortune_paragraph+refresh_button
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
